e6ac63e433651d13da940fd7c0ddd99bb9e8f938
Rhianna S <rhiannastuart@hotmail.co.uk>
2021-09-23 19:00:58 +0100

    set up site layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../pages/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 
 // Step 2: Define your component
@@ -8,6 +10,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Person watches war destroy city from afar.."
+        src="../images/disaster.jpg"
+      />
     </Layout>
   )
 }
